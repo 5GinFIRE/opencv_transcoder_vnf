58a2e82939dcb872d518c61e0129dd7f6735bdb2
Eduardo Sousa <eduardosousa@av.it.pt>
2018-02-26 14:48:45 +0000

    Fixing icons path. Updating gitignore

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ mkdir opencv_transcoder_vnf
 cp -r vnf/* opencv_transcoder_vnf
 cp -r charm opencv_transcoder_vnf
 cd opencv_transcoder_vnf
-find -mindepth 1 -type f -print | while read line; do md5sum $line >> checksums.txt; done
+find * -type f -print | while read line; do md5sum $line >> checksums.txt; done
 cd ..
 tar -czvf opencv_transcoder_vnf.tar.gz opencv_transcoder_vnf
 rm -rf opencv_transcoder_vnf
@@ -12,7 +12,7 @@ rm -rf opencv_transcoder_vnf
 mkdir opencv_transcoder_ns
 cp -r ns/* opencv_transcoder_ns
 cd opencv_transcoder_ns
-find -mindepth 1 -type f -print | while read line; do md5sum $line >> checksums.txt; done
+find * -type f -print | while read line; do md5sum $line >> checksums.txt; done
 cd ..
 tar -czvf opencv_transcoder_ns.tar.gz opencv_transcoder_ns
 rm -rf opencv_transcoder_ns
